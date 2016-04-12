6ce397bef6c4f268ab54502d39c942a51951918e
Steve White <srwhite@me.com>
2016-04-11 23:57:32 -0400

    Formatting cleanup.

--- smartapps/shackrat/wattvision-power-connector.src/wattvision-power-connector.groovy
+++ smartapps/shackrat/wattvision-power-connector.src/wattvision-power-connector.groovy
@@ -1,7 +1,7 @@
 /**
  *  Wattvision Connector
  *
- *  Copyright 2016 Steve White, based on PlottWatt Connector by Michael Kurtz
+ *  Copyright 2016 Steve White
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,54 +14,54 @@
  *
  */
 definition(
-    name: "Wattvision Power Connector",
-    namespace: "shackrat",
-    author: "Steve White",
-    description: "Uploads SmartThings Power Meter data to Wattvision",
-    category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/wattvision.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/wattvision%402x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+	name: "Wattvision Power Connector",
+	namespace: "shackrat",
+	author: "Steve White",
+	description: "Uploads SmartThings Power Meter data to Wattvision",
+	category: "My Apps",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/wattvision.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/wattvision%402x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 preferences {
-    section("Power Meter") {
-        input "power", "capability.powerMeter", title: "Select Power Meter", required: false, multiple: true
-    }
-    section ("Wattvision API") {
-        input "apiKey", "text", title: "Wattvision API Key", required:true
-        input "apiId", "text", title: "Wattvision API ID", required:true
-        input "sensorId", "text", title: "Wattvision Virtual Sensor ID", required:true
-    }
-    section ("General Settings") {
-        input "sensorHysteresis", "number", title: "Average Readings over (n) Seconds", required:true, defaultValue: "20"
-    }
+	section("Power Meter") {
+		input "power", "capability.powerMeter", title: "Select Power Meter", required: false, multiple: true
+	}
+	section ("Wattvision API") {
+		input "apiKey", "text", title: "Wattvision API Key", required:true
+		input "apiId", "text", title: "Wattvision API ID", required:true
+		input "sensorId", "text", title: "Wattvision Virtual Sensor ID", required:true
+	}
+	section ("General Settings") {
+		input "sensorHysteresis", "number", title: "Average Readings over (n) Seconds", required:true, defaultValue: "20"
+	}
 }
 
 def installed() {
-    initialize()
+	initialize()
 }
 
 def updated() {
-    unsubscribe()
-    initialize()
+	unsubscribe()
+	initialize()
 }
 
 def initialize() {
 	state.postInterval = sensorHysteresis.toInteger() * 1000
 	state.lastUpdate = now()
 	state.powerReadings = []
-    state.energyReading = 0
-    subscribe(power, "power", powerEvent)
-    subscribe(power, "energy", energyEvent)
+	state.energyReading = 0
+	subscribe(power, "power", powerEvent)
+	subscribe(power, "energy", energyEvent)
 }
 
 def powerEvent(evt) {
-    sendEvent(evt)
+	sendEvent(evt)
 }
 
 def energyEvent(evt) {
 	log.info "WPC logging energy reading of ${evt.value} watt-hours."
-    state.energyReading = evt.value.toFloat() * 1000
+	state.energyReading = evt.value.toFloat() * 1000
 }
 
 private sendEvent(evt) {
@@ -73,12 +73,12 @@ private sendEvent(evt) {
 		
 	def readings = state.powerReadings.size()
 	def watts = Math.round(state.powerReadings.sum() / readings)
-    def watthours = Math.round(state.energyReading)
+	def watthours = Math.round(state.energyReading)
 
-    log.info "WPC uploading ${watts}W based on ${readings} samples, ${watthours} watt-hours lifetime."
+	log.info "WPC uploading ${watts}W based on ${readings} samples, ${watthours} watt-hours lifetime."
 
 	state.powerReadings = []	 
-    state.lastUpdate = now()
+	state.lastUpdate = now()
 
 	def reqParams = [
 		uri: "https://www.wattvision.com/api/v0.2/elec",
@@ -86,12 +86,12 @@ private sendEvent(evt) {
 		requestContentType: "application/x-www-form-urlencoded;charset=utf-8"
 	]
 
-    httpPost(reqParams) { response ->
-    	if (response.status != 200 ) {
-        	log.error "Wattvision Power Connector logging failed."
-        	response.headers.each {
-                log.debug "${it.name} : ${it.value}"
-            }
-        }
+	httpPost(reqParams) { response ->
+		if (response.status != 200 ) {
+			log.error "Wattvision Power Connector logging failed."
+			response.headers.each {
+				log.debug "${it.name} : ${it.value}"
+			}
+		}
 	}
 }
