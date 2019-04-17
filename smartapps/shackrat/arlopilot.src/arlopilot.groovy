e8647c18345e1a41006dd368eaf5036b19e2ad9c
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2019-04-17 10:11:34 -0400

    Update arlopilot.groovy

--- smartapps/shackrat/arlopilot.src/arlopilot.groovy
+++ smartapps/shackrat/arlopilot.src/arlopilot.groovy
@@ -932,7 +932,7 @@ def about()
 	{
 		section()
 		{
-			paragraph image: "https://storage.googleapis.com/arlopilot/arlo-small.png", "Enhanced Arlo Integration v${appVersion}\nCopyright 2018, Steve White"
+			paragraph image: "https://farm9.staticflickr.com/8632/16461422990_e5121d68ee_o.jpg", "Enhanced Arlo Integration v${appVersion}\nCopyright 2018, Steve White"
 			if (versionCheck) paragraph title: "Update Available!", "An update to ArloPilot v${latestVersion} is available.", required: true
 		}
 		section()
